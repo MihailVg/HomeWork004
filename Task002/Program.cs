da61e3e08c5d501e7050a3cb44207605d9817ab4
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-08-05 00:39:37 +0300

    Task002 done

---
+++ Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача N2 Напишите метод, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Method(int A)
+{
+    int B = 10;
+    int result = 0;
+    while(A > B)
+    {
+        result = result + A%10;
+        A = A/10;
+    }
+    result = result + A;
+    Console.WriteLine(result);
+}
