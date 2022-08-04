af1dad2a88d6e654d13366c15c5ac4d230a14ce9
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-08-05 01:37:33 +0300

    Task003 done

---
+++ Task003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача N3 Напишите метод(ы), которай задаёт массив из 8 элементов и выводит их на экран.
+
+void Method1(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+void Method2(int[] array1)
+{
+    int count = 0;
+    int leng = array1.Length;
+
+    while(count < leng)
+    {
+        Console.Write(array1[count] + " ");
+        count++;
+    }
+}
+
+int[] array2 = new int[8];
+
+Method1(array2);
+Method2(array2);
